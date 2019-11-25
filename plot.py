91a16ccf3466b7ab9fbd9406f015bf7ce5ab169b
namanvashistha <namanvashistha15@gmail.com>
2019-11-26 02:17:46 +0530

    update

--- plot.py
+++ plot.py
@@ -1,8 +1,36 @@
+from sklearn.cluster import KMeans
 import pandas as pd
-quakes = pd.read_csv('crime.csv')
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+#import plotly.graph_objects as go
 
-import plotly.graph_objects as go
-fig = go.Figure(go.Densitymapbox(lat=quakes.Latitude, lon=quakes.Longitude, z=quakes.Magnitude, radius=10))
-fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+#%matplotlib inline
+
+df = pd.read_csv("crime.csv")
+#df.head()
+
+#plt.scatter(df.Latitude,df['Longitude'])
+#plt.xlabel('Latitude')
+#plt.ylabel('Longitude')
+
+#Elbow Plot
+
+sse = []
+K = []
+k_rng = range(1,50)
+for k in k_rng:
+    km = KMeans(n_clusters=k)
+    km.fit(df[['Latitude','Longitude']])
+    sse.append(km.inertia_)
+    K.append(k)
+
+plt.xlabel('K')
+plt.ylabel('Sum of squared error')
+axes= plt.axes()
+
+#axes.set_xlim([0,25])
+
+axes.set_xticks(K)
+plt.grid()
+plt.plot(k_rng,sse)
+#plt.scatter(k_rng,sse,color='green')
